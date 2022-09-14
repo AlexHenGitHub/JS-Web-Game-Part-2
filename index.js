2de920bf39861c2532a8a1db954ff86de62e7015
AlexHenGitHub <108377905+AlexHenGitHub@users.noreply.github.com>
2022-09-13 23:24:22 -0400

    changed font size on navbar

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ function newImage(url, left, bottom){
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', function(){
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
 }
 
+
+//adding item to inventory
+let inventory = document.createElement('div')
 function newInventory(){
-    let inventory = document.createElement('div')
+    
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
@@ -36,6 +45,15 @@ newImage('assets/pine-tree.png', 450, 350)
 newImage('assets/crate.png', 150, 350)
 newImage('assets/well.png', 500, 575)
 
+
+//removeable items
+newItem('assets/sword.png', 500, 555)
+newItem('assets/sword.png', 470, 555)
+newItem('assets/sword.png', 345, 123)
+newItem('assets/sword.png', 500, 555)
 newItem('assets/sword.png', 500, 555)
 newItem('assets/shield.png', 165, 335)
 newItem('assets/staff.png', 600, 250)
+newItem('assets/staff.png', 500, 250)
+newItem('assets/staff.png', 460, 250)
+newItem('assets/staff.png', 450, 560)
